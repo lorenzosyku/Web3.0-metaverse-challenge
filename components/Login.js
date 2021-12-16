e8734940a63f261e47af2cfda39dff925e560138
lorenzo syku <lorenzosyku@lorenzos-MacBook-Pro.local>
2021-12-16 10:23:15 -0500

    deleting comments

--- components/Login.js
+++ components/Login.js
@@ -7,16 +7,15 @@ function Login() {
 
   return (
     <div className="bg-black relative text-white">
-      <h1>i am login screen</h1>
       <div className="flex flex-col absolute z-10 w-full items-center justify-center h-4/6 space-y-4">
-        {/*papafam logo */}
+        
         <Image src="https://links.papareact.com/3pi"
           height={200}
           width={200}
           className=" object-cover rounded-full"
         />
 
-        {/*login btn */}
+        
         <button onClick={authenticate} className="bg-yellow-500 rounded-lg p-5 animate-pulse font-bold">Login into the METAVERSE</button>
       </div>
       <div className="w-full h-screen">
